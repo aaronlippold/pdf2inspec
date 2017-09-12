6629656e07dbd11e57bfece9f45596949933465f
HackerShark <melsharkawi@mitre.org>
2017-09-12 15:26:57 -0400

    parser.rb   - Parser can now parse sections without spacing inbetween!
    Huge thanks to @dromazmj for helping me to get this running properly :)

--- parser.rb
+++ parser.rb
@@ -65,10 +65,19 @@ class ControlParser < Parslet::Parser
     (space? >> word >> (space | dot).maybe).repeat(1)
   end
 
+  def line_body(ending)
+    (str(ending).absent? >> any).repeat(1)
+  end
+
+  def line(ending)
+    line_body(ending) >> eol?
+  end
+
+  def lines(ending)
+    line(ending).as(:line).repeat(1)
+  end
+
   rule(:eol?)       { str("\n").maybe }
-  rule(:line_body)  { (str("\n").absent? >> any).repeat(1) }
-  rule(:line)       { line_body >> eol? }
-  rule(:lines)      { line.as(:line).repeat(1) }
 
   rule :lparn do
     str('(')
@@ -85,7 +94,7 @@ class ControlParser < Parslet::Parser
         integer.repeat(1)
   end
   rule(:score) { lparn >> word >> rparn }
-  rule(:header) { (real.as(:section_num) >> words.as(:title) >> newline.maybe >> score.as(:score)).as(:header) >> newline >> newline}
+  rule(:header) { (real.as(:section_num) >> words.as(:title) >> newline.maybe >> score.as(:score)).as(:header) >> newline}
   rule :applicabilityValue do
     (word >>
         space >>
@@ -98,55 +107,47 @@ class ControlParser < Parslet::Parser
   rule :applicability do
     str('Profile Applicability:') >>
         newline >>
-        applicabilityValue >>
-        newline
+        applicabilityValue
   end
   rule :description do
     str('Description:') >>
         newline >>
-        lines.as(:description) >>
-        newline
+        lines('Rationale:').as(:description)
   end
   rule :rationale do
     str('Rationale:') >>
         newline >>
-        lines.as(:rationale) >>
-        newline
+        lines('Audit:').as(:rationale)
   end
   rule :audit do
     str('Audit:') >>
         newline >>
-        lines.as(:audit) >>
-        newline
+        lines('Remediation:').as(:audit)
   end
   rule :remediation do
     str('Remediation:') >>
         newline >>
-        lines.as(:remediation) >>
-        newline
+        lines('Impact:').as(:remediation)
   end
   rule :impact do
     str('Impact:') >>
         newline >>
-        lines.as(:impact) >>
-        newline
+        lines('Default Value:').as(:impact)
   end
   rule :default_value do
     str('Default Value:') >>
         newline >>
-        lines.as(:default_value) >>
-        newline
+        lines('References:').as(:default_value)
   end
   rule :references do
     str('References:') >>
         newline >>
-        lines.as(:references) >>
-        newline
+        lines('CIS Controls:').as(:references)
   end
   rule :cis_controls do
     str('CIS Controls:') >>
         newline >>
-        lines.as(:cis_controls) >>
+        lines("\n").as(:cis_controls) >>
         newline
   end
   rule :control do
@@ -161,45 +162,39 @@ class ControlParser < Parslet::Parser
         references >>
         cis_controls
   end
-  root :control
+  rule :controls do
+    control.repeat(1)
+  end
+  root :controls
 end
 
 testStr = '1.1 Ensure a separate partition for containers has been created (Scored)
-
 Profile Applicability:
 Level 1 - Linux Host OS
-
 Description:
 All Docker containers and their data and metadata is stored under /var/lib/docker
 directory. By default, /var/lib/docker would be mounted under / or /var partitions based
 on availability.
-
 Rationale:
 Docker depends on /var/lib/docker as the default directory where all Docker related files,
 including the images, are stored. This directory might fill up fast and soon Docker and the
 host could become unusable. So, it is advisable to create a separate partition (logical
 volume) for storing Docker files.
-
 Audit:
 At the Docker host execute the below command:
 grep /var/lib/docker /etc/fstab
 This should return the partition details for /var/lib/docker mount point.
-
 Remediation:
 For new installations, create a separate partition for /var/lib/docker mount point. For
 systems that were previously installed, use the Logical Volume Manager (LVM) to create
 partitions.
-
 Impact:
 None.
-
 Default Value:
 By default, /var/lib/docker would be mounted under / or /var partitions based on
 availability.
-
 References:
 1. https://www.projectatomic.io/docs/docker-storage-recommendation/
-
 CIS Controls:
 14 Controlled Access Based on the Need to Know
 Controlled Access Based on the Need to Know
